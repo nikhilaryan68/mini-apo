5faddf53725ef0de0209eaaf184adf04a0e40730
nikhilaryan68 <nikhilaryan68@gmail.com>
2026-06-15 01:00:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ from telegram.ext import (
 )
 
 # --- Configuration ---
-TOKEN = 8952610164:AAE852N52Om-axgF2RsXtovASRZP4sabzHw
+TOKEN = "8952610164:AAE852N52Om-axgF2RsXtovASRZP4sabzHw"
 if not TOKEN:
     raise ValueError("No BOT_TOKEN provided in environment variables!")
 
